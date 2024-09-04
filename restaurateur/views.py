a416f1b233a721e084bb55744b1039e69aa88bfb
Aleksey525 <alex_tolchin@mail.ru>
2024-09-04 17:03:21 +0300

    added calculation of distance to restaurants

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -6,9 +6,12 @@ from django.contrib.auth.decorators import user_passes_test
 
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
+import requests
 
 
 from foodcartapp.models import Product, Restaurant, Order, RestaurantMenuItem
+from geopy.distance import geodesic
+from django.conf import settings
 
 
 class Login(forms.Form):
@@ -63,6 +66,28 @@ def is_manager(user):
     return user.is_staff  # FIXME replace with specific permission
 
 
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lat, lon
+
+
+def calculate_distance(restaurant_coords, delivery_coords):
+    return geodesic(restaurant_coords, delivery_coords).kilometers
+
+
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_products(request):
     restaurants = list(Restaurant.objects.order_by('name'))
@@ -96,8 +121,9 @@ def view_orders(request):
         'elements__product__menu_items__restaurant').all()
 
     for order in order_items:
-        order_restaurants = set()
+        restaurant_dist = {}
         all_products_available = True
+        product_restaurants_sets = []
         for order_product in order.elements.all():
             product_restaurants = set()
             for menu_item in order_product.product.menu_items.all():
@@ -106,14 +132,29 @@ def view_orders(request):
             if not product_restaurants:
                 all_products_available = False
                 break
-            order_restaurants.update(product_restaurants)
+            product_restaurants_sets.append(product_restaurants)
         if all_products_available:
-            common_restaurants = set(order_restaurants)
-            for product_restaurants in [set(
-                menu_item.restaurant.name for menu_item in order_product.product.menu_items.all() if
-                menu_item.availability) for order_product in order.elements.all()]:
-                common_restaurants.intersection_update(product_restaurants)
+            common_restaurants = set.intersection(*product_restaurants_sets)
+            for restaurant_name in common_restaurants:
+                try:
+                    YANDEX_API_KEY = settings.YANDEX_API_KEY
+                    restaurant_coords = fetch_coordinates(YANDEX_API_KEY , restaurant_name)
+                    delivery_coords = fetch_coordinates(YANDEX_API_KEY, order.address)
+                    if restaurant_coords and delivery_coords:
+                        distance = calculate_distance(restaurant_coords, delivery_coords)
+                        restaurant_dist[restaurant_name] = distance
+                except Exception as error:
+                    print(f"Ошибка получения координат для {restaurant_name}: {error}")
+
             order.restaurants = common_restaurants
+
+        if restaurant_dist:
+            closest_restaurant = min(restaurant_dist, key=restaurant_dist.get)
+            order.closest_restaurant = closest_restaurant
+            order.closest_distance = restaurant_dist[closest_restaurant]
+        else:
+            order.closest_restaurant = None
+            order.closest_distance = None
 
     return render(request, template_name='order_items.html', context={
         'order_items': order_items,
