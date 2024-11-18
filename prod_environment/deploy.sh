6874809c5e8293bfda3269a244cbe87e50960b34
Aleksey525 <alex_tolchin@mail.ru>
2024-11-18 09:47:59 +0300

    edited deploy script and docker-compose file

--- prod_environment/deploy.sh
+++ prod_environment/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 echo "Сборка и запуск контейнеров..."
-docker compose build
-docker compose up -d
+
+docker compose up -d --build
 
 echo "Запуск Certbot для получения SSL сертификата..."
 docker compose run --rm certbot certonly --webroot --webroot-path=/var/www/certbot --email alex_tolchin@mail.ru --agree-tos --no-eff-email -d star-burg.ru -d www.star-burg.ru
 
-
 echo "Перезагрузка Nginx для применения новой конфигурации SSL..."
-
 docker compose exec nginx nginx -s reload
 
 if [ $? -ne 0 ]; then
